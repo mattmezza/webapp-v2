b335333ecd208f5032a2afc6f1657be42663e03f
Matteo Merola <mmerola@cleopa.de>
2016-05-26 17:16:09 +0200

    changed some colors

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'MyUnimol', 
+    student: {
+      name: 'Matteo Merola',
+      matr: '138654'
+    } });
 });
 
 module.exports = router;
